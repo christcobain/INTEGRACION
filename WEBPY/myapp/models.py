c804542755e3e300556f26a4e830f8d11a36d03b
bragonzu <33212907+bragonzu@users.noreply.github.com>
2024-04-26 22:52:52 -0500

    Rama braham (#2)
    * [ADD] Se agrego readme
    
    * [ADD] Se agrego models

--- WEBPY/myapp/models.py
+++ WEBPY/myapp/models.py
@@ -1,65 +1,86 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Rule(models.Model):
-    SELECTION_RULE = (
-        ('A', 'ADMIN'),
-        ('E', 'EMPLEADO'),
-        ('B', 'EMPLEADO_2'),
-    )
-    code = models.CharField(max_length=5)
-    name = models.CharField(max_length=10,choices=SELECTION_RULE)
+class Sede(models.Model):
+    nombre = models.CharField(max_length=50)
 
-    # Esto sirve en el administrador django se muestre la lista
-    # de project por su nombre
-    def __str__(self):
-        return self.name
+class Rol(models.Model):
+    nombre = models.CharField(max_length=50)
 
-class User(models.Model):
-    code = models.CharField(max_length=10)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    address = models.CharField(max_length=200)
-    phone = models.IntegerField()
-    edad = models.IntegerField()
-    rule = models.ForeignKey(Rule, on_delete=models.CASCADE)
-     
-    def __str__(self):
-        return self.first_name + '  -  ' + self.last_name
+class UnidadOrganica(models.Model):
+    nombre = models.CharField(max_length=50)    
 
+class Estado(models.Model):
+    nombre = models.CharField(max_length=50)   
+    
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    dni = models.CharField(max_length=10)
+    unidadOrganica = models.ForeignKey(UnidadOrganica, on_delete=models.CASCADE)
+    sede = models.ForeignKey(Sede, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    contrasena = models.CharField(max_length=20)
 
+class Funcionario(models.Model):
+    nombre = models.CharField(max_length=50)
+    campo = models.CharField(max_length=20)
+    unidadOrganica = models.ForeignKey(UnidadOrganica, on_delete=models.CASCADE)
 
+class Movimiento(models.Model):
+    nombre = models.CharField(max_length=50)
 
-class Category(models.Model):
-    code = models.CharField(max_length=10)
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+class Situacion(models.Model):
+    nombre = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name 
-        
-class Product(models.Model):
-    SELECTION_TYPE = (
-        ('L', 'LITROS'),
-        ('M', 'METROS'),
-        ('V', 'VOLUMEN'),
-        ('U', 'UNIDAD'),
-    )
-    code = models.CharField(max_length=10)
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price_unit = models.FloatField() 
-    quantity = models.FloatField()
-    type_product = models.CharField(max_length=1, choices=SELECTION_TYPE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    done = models.BooleanField(default=False)
+class Proceso(models.Model):
+    movimiento = models.ForeignKey(Movimiento, on_delete=models.CASCADE)
+    fechaIngreso = models.CharField(max_length=20)
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    situacion = models.ForeignKey(Situacion, on_delete=models.CASCADE)
+    fechaEliminacion = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.name + '  -  ' + self.category.name
+class Descripcion(models.Model):
+    nombre = models.CharField(max_length=50)
 
 
+class Bien(models.Model):
+    descripcion = models.ForeignKey(Descripcion, on_delete=models.CASCADE)
+    ordenCompra = models.IntegerField()
+    proveedor = models.CharField(max_length=20)
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    serie = models.CharField(max_length=20)
+    fechaVenGarantia = models.CharField(max_length=20)
+    componentes = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    situacion = models.ForeignKey(Situacion, on_delete=models.CASCADE)
 
+class DetalleTransferencia(models.Model):
+    motivo = models.CharField(max_length=100)
+    observaciones = models.CharField(max_length=100)
 
-class MOvUser_Product(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+class Asunto(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class DetalleSalida(models.Model):
+    asunto = models.ForeignKey(Asunto, on_delete=models.CASCADE)
+    antecedentes = models.CharField(max_length=200)
+    analisis = models.CharField(max_length=1000)
+    conclusiones = models.CharField(max_length=300)
+    recomendaciones = models.CharField(max_length=200)
+
+class DetalleProceso(models.Model):
+    proceso = models.ForeignKey(Proceso, on_delete=models.CASCADE)
+    UsuarioInicial = models.CharField(max_length=20)
+    unidadOrganicaInicial = models.CharField(max_length=20)
+    sedeInicial = models.CharField(max_length=20)
+    UsuarioFinal = models.CharField(max_length=20)
+    unidadOrganicaFinal = models.CharField(max_length=20)
+    sedeFinal = models.CharField(max_length=20)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    bien = models.ForeignKey(Bien, on_delete=models.CASCADE)
+    detalleTransferencia = models.ForeignKey(DetalleTransferencia, on_delete=models.CASCADE)
+    detalleSalida = models.ForeignKey(DetalleSalida, on_delete=models.CASCADE)
+
